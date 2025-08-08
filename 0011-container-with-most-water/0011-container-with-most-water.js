a8adc078c1b1289ba977f0973ed71976ae091a41
Han Juin Wong <hjwong12@gmail.com>
2025-08-09 00:32:43 +1200

    Time: 1851 ms (5.3%), Space: 75.7 MB (5.19%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let i = 0
+    let j = height.length - 1
+    let max = 0
+    let area = 0
+    while (i < j ){
+        if(height[i] < height[j]){
+            area = height[i] * (j-i)
+            if(height.length == 2){
+                area = height[i]
+            }
+            i++
+        }else{
+            area = height[j] * (j-i)
+            if(height.length == 2){
+                area = height[j]
+            }
+            j--
+        }
+        if(area > max){
+            max = area
+        }
+        console.log('max' + max)
+        console.log('area' + area)
+    }
+    return max
+};
