7a661120a4dbb5f9267822728a0eb35237777656
Han Juin Wong <hjwong12@gmail.com>
2025-08-08 18:08:20 +1200

    Time: 3 ms (18.59%), Space: 54.7 MB (75.03%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let i = 0
+    let j = 0
+    while (i < s.length && j < t.length){
+        if(s[i] == t[j]){
+            i ++
+            j ++
+        }else{
+            j ++
+        }
+    }
+    console.log(i)
+    if(i == s.length ){return true}
+    return false
+};
