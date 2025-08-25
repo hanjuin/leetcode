35a07c704b335621ce4055588fca82202b97933f
Han Juin Wong <hjwong12@gmail.com>
2025-08-26 11:58:00 +1200

    Time: 3 ms (30.95%), Space: 57.7 MB (6.19%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -6,20 +6,19 @@ var longestCommonPrefix = function(strs) {
     let shortest = strs[0]
     let prefix = ''
 
-    for(let word in strs){
-        if(strs[word].length < shortest.length){
-            shortest = strs[word]
+    for(let word of strs){
+        if(word.length < shortest.length){
+            shortest = word
         }
     }
 
     for (let i = 0; i < shortest.length; i++) {
-        let flag = true
+        
         for (let word of strs) {
             if (word[i] !== shortest[i]) {
-                flag = false
+                return prefix
             }
         }
-        if(flag == false){break}
         prefix += shortest[i]
     }
 
